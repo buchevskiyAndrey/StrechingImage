e2d0c40bb44ae27957972855418be829a463917b
Andrey Buchevskiy <aabuchevsk@mts.ru>
2024-04-20 09:49:37 +0300

    Initial Commit

---
+++ StrechingImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrechingImage
+//
+//  Created by Бучевский Андрей on 20.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
